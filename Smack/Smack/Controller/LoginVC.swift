6d98d264aac64c7a96cdda3bfebba78ccb5d6643
Simon Lovelock <simon@simons-mbp.home>
2019-01-15 20:24:31 +0000

    Login View added

---
+++ Smack/Smack/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Simon Lovelock on 15/01/2019.
+//  Copyright © 2019 Simon Lovelock. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Simon Lovelock on 15/01/2019.
+//  Copyright © 2019 Simon Lovelock. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
